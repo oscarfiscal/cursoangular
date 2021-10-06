01fff1464f3edab942cfde68dd899b3375d9cc4d
oscarfiscal <oscarfiscalcp@gmail.com>
2021-10-06 17:06:49 -0500

    two way binding

--- src/app/Personas/Personas.component.ts
+++ src/app/Personas/Personas.component.ts
@@ -11,11 +11,11 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
 deshabilitar = false;
 mensaje = 'No se ha agregado ninguna persona';
-titulo ='';
+titulo ='Ingeniero';
 agregarPersona(){
     this.mensaje= 'Persona Agregada';
 }
-modificarTitulo(event:Event){
-this.titulo= (<HTMLInputElement>event.target).value;
-}
+// modificarTitulo(event:Event){
+// this.titulo= (<HTMLInputElement>event.target).value;
+// }
 }
